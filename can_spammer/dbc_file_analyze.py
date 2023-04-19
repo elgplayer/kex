7c8a4b06e61f5081f124469e0cae66db822f80bf
Carl Elg <carlval@hotmail.se>
2023-04-19 22:35:54 +0200

    Now normalizing supported

--- can_spammer/dbc_file_analyze.py
+++ can_spammer/dbc_file_analyze.py
@@ -17,15 +17,18 @@ from tqdm import tqdm
 from canlib import canlib, Frame
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 db = cantools.database.load_file(r"src/can1.dbc")
 
 #### CONFIG ###
 
-normalize_frame_id = False
+normalize_frame_id = True
 
 ###############
 
+global messages_name
+global messages_frame_id
 
 ignored_messages = []
 messages_frame_id = []
@@ -34,8 +37,8 @@ messages_name     = []
 ignore_id = [1280, 17]
 
 for k,message in enumerate(db.messages):
-    if message.is_multiplexed():
-        continue
+    # if message.is_multiplexed():
+    #     continue
     
     frame_id = message.frame_id
     
@@ -55,6 +58,49 @@ sorted_zipped_lists = sorted(zipped_lists, key=lambda x: x[0])
 # Unzip the sorted lists back into individual lists
 messages_frame_id, messages_name = zip(*sorted_zipped_lists)
 
+if normalize_frame_id: 
+    messages_frame_id = range(0,len(messages_frame_id))
+    
+messages_name = list(messages_name)
+messages_frame_id = np.array(messages_frame_id)
+
+def insert_at_percentage(perc, how='under', print_messages=True):
+    global messages_name
+    global messages_frame_id
+    
+    mapping_list = {}
+    len_list = len(messages_name) + 2
+
+    if how == 'under':
+        idx = math.floor(len_list * perc / 100) - 1
+    else:
+        idx =  math.floor(len_list * perc / 100) + 1
+
+    messages_name.insert(idx, '--- RESEVERED STM ---')
+    messages_name.insert(idx, '--- RESEVERED VESC ---')
+
+    messages_frame_id = np.insert(messages_frame_id, idx, idx)
+    messages_frame_id = np.insert(messages_frame_id, idx+1, idx+1)
+    messages_frame_id = messages_frame_id[idx+2:] + 2
+
+    if print_messages:
+        for k in range(len_list):
+            message = messages_name[k].ljust(35)
+            percentage = round(k / len(messages_name) * 100, 1)
+            print(f"{message} - {k} / {len(messages_name)-1} ({percentage}) %")
+            mapping_list[message] = k
+          
+    if print_messages:
+        pprint.pprint(mapping_list)
+            
+            
+insert_at_percentage(22, 'over')
+
+
+
+#%%
+# for k,x in enumerate(messages_frame_id):
+#     print(messages_frame_id[k], messages_name[k])
 
 percentiles = np.arange(0, 100, 10)  # [90, 80, 70, ..., 0]
 indices = []
@@ -64,27 +110,20 @@ for p in percentiles:
     index = np.searchsorted(messages_frame_id, value)
     indices.append(index)
     
-if normalize_frame_id: 
-    messages_frame_id = range(0,len(messages_frame_id))
 
-# for k,x in enumerate(messages_frame_id):
-#     print(messages_frame_id[k], messages_name[k])
-
-plt.figure(figsize=(16, 8))
+plt.figure(figsize=(10, 8))
 plt.plot(messages_frame_id, marker='x')
 plt.xlabel("Index")
 plt.ylabel("Frame ID")
 
-
 for k, x in enumerate(indices):
     percentile = percentiles[k]
     frame_id = messages_frame_id[x]
-    message_name = messages_name[x]
+    _message_name = messages_name[x]
     
     plt.axvline(x=x, color='r', linestyle='--', alpha=0.7)
-    plt.text(x, 0, f"{frame_id} ({message_name})", rotation=90, ha='right', va='bottom')
+    plt.text(x, 0, f"{frame_id} ({_message_name})", rotation=90, ha='right', va='bottom')
     
-
 plt.title(f"Frame IDs with Percentile Lines | Normalized: {normalize_frame_id}")
 plt.grid(True)
 plt.show()
--- can_spammer/generate_CAN_messages.py
+++ can_spammer/generate_CAN_messages.py
@@ -23,8 +23,65 @@ ch_send.setBusOutputControl(canlib.canDRIVER_NORMAL)
 ch_send.setBusParams(canlib.canBITRATE_500K)
 ch_send.busOn()
 
-db = cantools.database.load_file(r"can1.dbc")
+db = cantools.database.load_file(r"src/can1.dbc")
 random.seed(42)
+
+
+mapping_list = {
+'---RESEVEREDSTM---':13,
+'---RESEVEREDVESC---':12,
+'FSE_EnergyMeter_Data':48,
+'SBG_ECAN_MSG_AUTO_SLIP_CURV':47,
+'SBG_ECAN_MSG_EKF_EULER':37,
+'SBG_ECAN_MSG_EKF_INFO':36,
+'SBG_ECAN_MSG_EKF_ORIENTATION_ACC':38,
+'SBG_ECAN_MSG_EKF_POS':39,
+'SBG_ECAN_MSG_EKF_POS_ACC':40,
+'SBG_ECAN_MSG_EKF_VEL_NED':41,
+'SBG_ECAN_MSG_EKF_VEL_NED_ACC':42,
+'SBG_ECAN_MSG_GPS1_HDT':45,
+'SBG_ECAN_MSG_GPS1_HDT_INFO':44,
+'SBG_ECAN_MSG_IMU_ACCEL':34,
+'SBG_ECAN_MSG_IMU_GYRO':35,
+'SBG_ECAN_MSG_IMU_INFO':33,
+'SBG_ECAN_MSG_ODO_VEL':43,
+'ams_cell_temperatures':8,
+'ams_cell_voltages':7,
+'ams_error':4,
+'ams_state':5,
+'ams_status_1':6,
+'ams_temperatures':9,
+'ccu_status_1':24,
+'ccu_status_2':23,
+'dbu_status_1':19,
+'dbu_status_2':20,
+'dcu_pps':1,
+'dcu_status':2,
+'dv_control_target':3,
+'dv_driving_dynamics_2':49,
+'dv_system_status':50,
+'ebs_status':22,
+'ecu_status':21,
+'fault':32,
+'lv_bms_data_a':27,
+'lv_bms_data_b':29,
+'lv_bms_status_a':28,
+'lv_bms_status_b':30,
+'lv_power_signal':26,
+'mcu_set_ccu_cooling_points':25,
+'mcu_set_dbu_indicator_points':52,
+'mcu_set_ecu_indicator_points':51,
+'res_initialization':53,
+'res_ntm_node_control':0,
+'res_status':46,
+'set_mcu_inverter_points':31,
+'swu_status':18,
+'vehicle_status':10,
+'vehicle_status_wheel_speed':11,
+'vmu_fl':14,
+'vmu_fr':15,
+'vmu_rl':16,
+'vmu_rr':17}
 
 def generate_random_value_for_signal(signal):
     
@@ -101,10 +158,16 @@ def process_item(i):
                 random_message[key] = 0
         encoded_msg = message.encode(random_message, scaling=False)
         decoded_msg = message.decode(encoded_msg, scaling=False)
+        frame_id = message.frame_id
+        
+        if message.name in mapping_list:
+            frame_id = mapping_list[message.name]
+        else:
+            continue 
 
         random_data[message.name] = {
             'name': message.name,
-            'frame_id': message.frame_id,
+            'frame_id': frame_id,
             'data': random_message,
             'encoded_data': encoded_msg,
             'decoded_data': decoded_msg
@@ -114,34 +177,11 @@ def process_item(i):
 
 if __name__ == '__main__':
 
-
     freeze_support() # Add this line to fix the RuntimeError
 
     data_list = []
-    nr_of_messages = 100000
+    nr_of_messages = 1000
     my_list = range(nr_of_messages)
-
-    def process_item(i):
-        random_data = {}
-        for k, message in enumerate(db.messages):
-            if message.is_multiplexed():
-                continue
-            random_message = generate_random_signals_for_message(message)
-            # Wipe
-            for key, value in random_message.items():
-                if value < 0:
-                    random_message[key] = 0
-            encoded_msg = message.encode(random_message, scaling=False)
-            decoded_msg = message.decode(encoded_msg, scaling=False)
-
-            random_data[message.name] = {
-                'name': message.name,
-                'frame_id': message.frame_id,
-                'data': random_message,
-                'encoded_data': encoded_msg,
-                'decoded_data': decoded_msg
-            }
-        return random_data
 
     # create a pool of worker processes
     with Pool(processes=8) as pool:
@@ -151,18 +191,20 @@ if __name__ == '__main__':
     # store the results in a list
     for result in results:
         data_list.append(result)
+        
+    pprint.pprint(data_list[0:10])
 
     # open a file for binary writing
     with open('output/messages_small.pkl', 'wb') as file:
         # dump the list into the file
         pickle.dump(data_list[0:1000], file)
 
-    with open('output/messages_medium.pkl', 'wb') as file:
-        # dump the list into the file
-        pickle.dump(data_list[0:10000], file)
+    # with open('output/messages_medium.pkl', 'wb') as file:
+    #     # dump the list into the file
+    #     pickle.dump(data_list[0:10000], file)
 
-    with open('output/messages_large.pkl', 'wb') as file:
-        # dump the list into the file
+    # with open('output/messages_large.pkl', 'wb') as file:
+    #     # dump the list into the file
         pickle.dump(data_list, file)
 
     print("Files pickled!")
